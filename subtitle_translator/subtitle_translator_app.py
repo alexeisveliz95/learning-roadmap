6fc0fd5a81579d866b43404318176c2db814cb67
Alexeis Veliz <alexeisveliz95@gmail.com>
2026-04-25 04:44:41 +0000

    25 abr 2026, 0:44

--- subtitle_translator/subtitle_translator_app.py
+++ subtitle_translator/subtitle_translator_app.py
@@ -1,1 +1,8 @@
 # Subtile translator CLI
+import typer
+from rich.console import Console
+from parse import parse_srt
+from translator import translate_batches
+
+app = typer.Typer()
+console = Console()
