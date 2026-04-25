f7dc1bdf259ba919e7bf17a384ec99fec3e4e2ae
Alexeis Veliz <alexeisveliz95@gmail.com>
2026-04-25 04:02:32 +0000

    25 abr 2026, 0:02

--- subtitle_translator/parse.py
+++ subtitle_translator/parse.py
@@ -20,4 +20,19 @@ def parse_srt(file_path: str)-> List(SubtitleBlock):
 
     with open(file_path, "r", encoding="utf-8") as file:
         current_index, current_time, current_text = "", "", []
+
+        for line in file:
+            line = line.strip()
+            if not line:
+                if current_index and current_time:
+                    blocks.append(SubtitleBlock(current_index, current_time, current_text))
+                    current_index, current_time, current_text = "", "", []
+                continue
+
+            if time_mark_detection(line):
+                current_time = line
+            elif line.isdigit() and not current_time:
+                current_index = line
+            else:
+                current_text.append(line)
         
