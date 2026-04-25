b3f9b556e4ad3c65b95a9a11fbb1e88a18d59de4
Alexeis Veliz <alexeisveliz95@gmail.com>
2026-04-25 04:54:47 +0000

    25 abr 2026, 0:54

--- CLI_Password_Generator/test_password_gen.py
+++ CLI_Password_Generator/test_password_gen.py
@@ -1,6 +1,14 @@
 from password_gen import generate_password
 
 def test_password_length():
-    profile = {"length": 16}
-    resultado = generate_password(profile)
-    assert len(resultado) == 16
+    dict_config = {
+        "length": 8,
+        "use_uppercase": True,
+        "use_digits": True,
+        "use_special_chars": True,
+        "avoid_ambiguous": True,
+        "minimum_uppercase": 1,
+        "minimum_digits": 1,
+        "minimum_special_chars": 1
+    }
+    assert generate_password(dict_config) == 8
